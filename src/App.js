4f2d056fc27e3ecf6ff034cf7696f392b0bf021f
AgentBoo <zuzi.han@gmail.com>
2018-02-04 19:47:07 -0500

    changes to readme

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React, React DnD, and Redux example </h1>
+          <h1 className="App-title">Welcome to React DnD and Redux example </h1>
         </header>
         <main className="App-main">
           <Board knightPosition={ knightPosition }/>
